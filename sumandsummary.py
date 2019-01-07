617d38b1fe74ca55aed8721abe8cb384ca2d4116
Saiful Islam <tosaiful@live.com>
2019-01-07 20:00:33 +0100

    sum and summary added
    A simple way to calculate the sum and summarize.

---
+++ sumandsummary.py
@@ -0,0 +1,17 @@
+myList = [1,2,3,4,5,6,7]
+listSum = 0
+for num in myList:
+    listSum += num
+    print (listSum)
+    
+print (listSum / len(myList))
+
+
+myList = [1,2,3,4,5,6,7]
+listSum = 0
+for num in myList:
+    listSum += num
+
+print (listSum)
+    
+print (listSum / len(myList))
