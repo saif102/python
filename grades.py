c210bdc472d6fd835a88babc0c011c2e24a899d1
Saiful Islam <tosaiful@live.com>
2019-01-15 13:38:12 +0100

    updated with auto input.

--- grades.py
+++ grades.py
@@ -1,26 +1,32 @@
-grade = 76
+print("Enter your points:")
 
-if grade>= 95:
+x = float(input())
+
+if x>= 95:
     letterGrade = "1,0 (sehr gut / excellent)"
-elif grade>= 90:
+elif x>= 90:
     letterGrade = "1,3 (sehr gut / excellent)"
-elif grade>= 85:
+elif x>= 85:
     letterGrade = "1,7 (gut / good)"
-elif grade>= 80:
+elif x>= 80:
     letterGrade = "2,0 (gut / good)"
-elif grade>= 75:
+elif x>= 75:
     letterGrade = "2,3 (gut / good)"
-elif grade>= 70:
+elif x>= 70:
     letterGrade = "2,7 (befriedigend / satisfactory)"
-elif grade>= 65:
+elif x>= 65:
     letterGrade = "3,0 (befriedigend / satisfactory)"
-elif grade>= 60:
+elif x>= 60:
     letterGrade = "3,3 (befriedigend / satisfactory)"
-elif grade>= 55:
+elif x>= 55:
     letterGrade = "3,7 (ausreichend / sufficient)"
-elif grade>= 50:
+elif x>= 50:
     letterGrade = "4,0 (ausreichend / sufficient)"
 else:
     letterGrade = "5,0 (nicht bestanden / failed)"
 
-print (letterGrade)
+if x>= 50:
+    print ("Congratulations! Your result is: " + letterGrade + ".")
+else:
+    print (letterGrade + ". Sorry! Your point is not enough. Better luck next time.")
+
